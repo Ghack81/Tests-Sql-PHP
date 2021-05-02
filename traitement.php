e1b3e4f8a741c386dc651003f06fdc8ea7999023
Ghack81 <herve-mention@hotmail.com>
2021-05-02 17:29:41 +0200

    Version 1.1

--- traitement.php
+++ traitement.php
@@ -6,6 +6,7 @@
     </head>
     <body>
         <?php
+		//Step 1
    // Vérifier si le formulaire est soumis 
    if ( isset( $_POST['submit'] ) ) {
      /* récupérer les données du formulaire en utilisant 
@@ -17,10 +18,13 @@
         $age = $_POST["age"];
         $sexe = $_POST["sexe"];
         $comments = $_POST["comments"];
+		//Step 2
      // afficher le résultat
      echo '<h3>Informations récupérées en utilisant POST</h3>'; 
      echo 'Nom : ' . $nom . ' Age : ' . $age . ' Adresse : ' . $adresse . ' Téléphone' . $telephone . ' Email' . $email . ' sexe ' . $sexe . ' comments '. $comments ; 
      exit;
+	 //Step 3
+	 //Insertion des données dans une BDD et afficher
   }
 ?>
     </body>
