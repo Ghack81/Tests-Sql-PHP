4492e96b20966c6dc38978ff00ec7e4db7a23a36
Ghack81 <herve-mention@hotmail.com>
2022-03-19 21:46:16 +0100

    Add foncion PDO

--- connexion.php
+++ connexion.php
@@ -1,11 +1,26 @@
 <?php
 // Fonction PDO pour connexion
-
 // Préparation de la requète sql
-
-
 // Exécution de/des fonctions
-
 // Fermeture connexion
-
+require("connect.php");
+// pour oracle: $dsn="oci:dbname=//serveur:1521/base
+// pour sqlite: $dsn="sqlite:/tmp/base.sqlite"
+$dsn="mysql:dbname=".BASE.";host=".SERVER;
+    try{
+      $connexion=new PDO($dsn,USER,PASSWD);
+    }
+    catch(PDOException $e){
+      printf("Échec de la connexion : %s\n", $e->getMessage());
+      exit();
+    }
+$sql="SELECT * from CARNET";
+if(!$connexion->query($sql)) echo "Pb d'accès au CARNET";
+else{
+     foreach ($connexion->query($sql) as $row)
+     echo $row['PRENOM']." ".
+          $row['NOM']."né(e) le ".
+          $row['NAISSANCE']."<br/>\n";
+}
+?>
 ?>
