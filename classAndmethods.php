746cd32e64b85d0833e0b5933b1a6efdffc080e4
Ghack81 <herve-mention@hotmail.com>
2022-03-25 12:38:35 +0100

    File V1.2

--- classAndmethods.php
+++ classAndmethods.php
@@ -1,6 +1,21 @@
 <?php
-
-
-
-
+class Voiture {
+     // Attributs
+     private $vitesse;
+     private $model;
+     // Constructeur de la classe
+     function __construct($model) {
+          $this->model = $model;
+     }
+     // Methods
+     public function accelerer() {
+          // ajoute 10 miles par heure à la vitesse actuelle
+          $vitesse = $vitesse + 10;
+     }
+  
+     public function freiner() {
+          // déduire 10 miles par heure à la vitesse actuelle    
+          $vitesse = $vitesse - 10;
+     }
+}
 ?>
