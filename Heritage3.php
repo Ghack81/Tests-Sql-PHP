04a88e4611a867a65c757c952b0207caf59a9299
Ghack81 <herve-mention@hotmail.com>
2022-03-25 12:56:48 +0100

    Example 3 for heritage

---
+++ Heritage3.php
@@ -0,0 +1,41 @@
+<?php
+    // classe mére
+    class Vehicule {
+        // propriété protégé
+        protected $marque;
+        
+        // fonction publique
+        public function accelerer() {
+            echo $this->marque. " accélère...\n";
+        }
+        
+        // fonction publique
+        public function freiner() {
+            echo $this->marque. " a freiné...\n";
+        }
+    
+        // fonction publique pour modifier la propriété marque
+        public function setMarque($marque)
+        {
+            $this->marque = $marque;
+        }
+    }
+    
+    // classe fille
+    class Voiture extends Vehicule {
+  // méthode de la classe fille  
+        public function conduire() {
+            echo "Marque : " . $this->marque . "\n";
+            echo "Bonne conduite...\n";
+        }
+    
+    }
+    $voiture = new Voiture();
+    $voiture->setMarque("Audi");
+    // appeler la méthode de la classe voiture
+    $voiture->conduire();
+  
+    // appeler les méthodes de la classe véhicule
+    $voiture->accelerer();
+    $voiture->freiner();
+?>
